c78e3f332f0da959350b6bb7919596e0c1f6b816
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2022-06-10 14:50:52 +0300

    Created temperature vector. Solved the system.

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,6 +1,7 @@
 # %%
 import numpy as np
-from scipy.sparse import csr_array
+from scipy.sparse import lil_array, csr_array
+from scipy.sparse.linalg import spsolve
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -12,7 +13,7 @@ def get_connection_matrix(
     other_weight=-1.0,
     directions=((0,1), (0, -1), (1, 0), (-1, 0)),
 ):
-    conmat = csr_array((m*n, m*n), dtype=np.float64)
+    conmat = lil_array((m*n, m*n), dtype=np.float64)
     conmat.setdiag(self_weight)
     for i in range(m):
         for j in range(n):
@@ -22,10 +23,29 @@ def get_connection_matrix(
                     x = i*n + j
                     y = k*n + l
                     conmat[x, y] = other_weight
-    return conmat
+    return conmat.tocsr()
                 
+                
+def external_heat_func(x, y, mu=0.05, sigma=0.005, seed=None):
+    assert np.shape(x) == np.shape(y)
+    shape = np.shape(x)
+    rng = np.random.default_rng(seed)
+    r = rng.normal(mu, sigma, shape)
+    val = 100 * np.exp(
+        -((x - 0.55)**2 + (y - 0.45)**2) / r
+    )
+    return val
 
-conmat = get_connection_matrix(4, 4)
+
+def get_temperatures(minval, maxval, step):
+    xvals = yvals = np.arange(minval+step, maxval, step)  # h, 2h, ... 1-h
+    x_mesh, y_mesh = np.meshgrid(xvals, yvals)
+    z = external_heat_func(x_mesh, y_mesh)
+    return z.flatten(order='C')
 
 
-# %%
+conmat = get_connection_matrix(39, 39)
+temps = get_temperatures(0, 1, 1/40)
+solution = spsolve(conmat, temps)
+    
+        
